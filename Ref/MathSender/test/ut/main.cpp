7961941bb5f0d9444042eaf01d6ce11bda2dd6c7
Sean Marquez <capsulecorplab@gmail.com>
2022-02-15 13:43:49 -0700

    add TEST macro in main.cpp

--- Ref/MathSender/test/ut/main.cpp
+++ Ref/MathSender/test/ut/main.cpp
@@ -1,5 +1,10 @@
 #include "Tester.hpp"
 
+TEST(Nominal, AddCommand) {
+    Ref::Tester tester;
+    tester.testAddCommand();
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
